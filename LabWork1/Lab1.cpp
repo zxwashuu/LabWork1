7fb33c51963beb4e019ae4939e8797746913a593
Ярослав Дідусенко <45487284+zxwashuu@users.noreply.github.com>
2023-01-10 01:48:12 +0200

    Push files via PC

---
+++ LabWork1/Lab1.cpp
@@ -0,0 +1,12 @@
+
+#include <iostream>
+void main()
+{
+	using namespace std::string;
+        using namespace std::cin;
+        using namespace std::cout;
+	string name;
+	cout << "Enter your name!\n";
+	cin >> name;
+	cout << "Your name is " << name;
+}
